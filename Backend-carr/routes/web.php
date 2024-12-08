291519614a552933434134aaf5750fdf2c6d0cfd
Gerardo <torresanibal388@gmail.com>
2024-12-08 18:26:37 -0400

    Cambios de la logica del frontend

--- Backend-carr/routes/web.php
+++ Backend-carr/routes/web.php
@@ -2,9 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
+use function Pest\Laravel\patch;
+
 Route::get('/', function () {
-    return view('welcome');
+return response()->file(base_path('public/frontend/index.html'));
 });
+
+Route::get('/{any}', function ($any){
+    $path = base_path("public/frontend/$any");
+    if (file_exists($path)){
+        return response()->file($path);
+    }
+    abort(404);
+})->where('any','.*');
 
 Route::middleware([
     'auth:sanctum',
